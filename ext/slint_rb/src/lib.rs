20a6b8cf233c6b86edc3cefc8ce0c30f1c0e98f7
Wojciech Zygmuntowicz <wzygmuntowicz.zygmuntowicz@gmail.com>
2026-06-24 20:13:20 +0000

    Format lib.rb

--- ext/slint_rb/src/lib.rs
+++ ext/slint_rb/src/lib.rs
@@ -9,48 +9,20 @@ fn init(ruby: &Ruby) -> Result<(), Error> {
 
     let compiler_class = module.define_class("Compiler", ruby.class_object())?;
     compiler_class.define_singleton_method("new", function!(compiler::Compiler::new, 0))?;
-    compiler_class.define_method(
-        "build_from_path",
-        method!(compiler::Compiler::build_from_path, 1),
-    )?;
-    compiler_class.define_method(
-        "build_from_source",
-        method!(compiler::Compiler::build_from_source, 2),
-    )?;
-    compiler_class.define_method(
-        "include_paths",
-        method!(compiler::Compiler::include_paths, 0),
-    )?;
-    compiler_class.define_method(
-        "include_paths=",
-        method!(compiler::Compiler::set_include_paths, 1),
-    )?;
-    compiler_class.define_method(
-        "library_paths",
-        method!(compiler::Compiler::library_paths, 0),
-    )?;
-    compiler_class.define_method(
-        "library_paths=",
-        method!(compiler::Compiler::set_library_paths, 1),
-    )?;
+    compiler_class.define_method("build_from_path", method!(compiler::Compiler::build_from_path, 1))?;
+    compiler_class.define_method("build_from_source", method!(compiler::Compiler::build_from_source, 2))?;
+    compiler_class.define_method("include_paths", method!(compiler::Compiler::include_paths, 0))?;
+    compiler_class.define_method("include_paths=", method!(compiler::Compiler::set_include_paths, 1))?;
+    compiler_class.define_method("library_paths", method!(compiler::Compiler::library_paths, 0))?;
+    compiler_class.define_method("library_paths=", method!(compiler::Compiler::set_library_paths, 1))?;
     compiler_class.define_method("style", method!(compiler::Compiler::style, 0))?;
     compiler_class.define_method("style=", method!(compiler::Compiler::set_style, 1))?;
 
     let compilation_result_class = module.define_class("CompilationResult", ruby.class_object())?;
-    compilation_result_class
-        .define_method("valid?", method!(compiler::CompilationResult::valid, 0))?;
-    compilation_result_class.define_method(
-        "diagnostics",
-        method!(compiler::CompilationResult::diagnostics, 0),
-    )?;
-    compilation_result_class.define_method(
-        "component_names",
-        method!(compiler::CompilationResult::component_names, 0),
-    )?;
-    compilation_result_class.define_method(
-        "components",
-        method!(compiler::CompilationResult::components, 0),
-    )?;
+    compilation_result_class.define_method("valid?", method!(compiler::CompilationResult::valid, 0))?;
+    compilation_result_class.define_method("diagnostics", method!(compiler::CompilationResult::diagnostics, 0))?;
+    compilation_result_class.define_method("component_names", method!(compiler::CompilationResult::component_names, 0))?;
+    compilation_result_class.define_method("components", method!(compiler::CompilationResult::components, 0))?;
 
     let diagnostic_class = module.define_class("Diagnostic", ruby.class_object())?;
     diagnostic_class.define_method("message", method!(compiler::Diagnostic::message, 0))?;
