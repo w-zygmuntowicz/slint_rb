16507fb75ac281e0155ea2803f509cb0e3ff2f5f
Wojciech Zygmuntowicz <wzygmuntowicz.zygmuntowicz@gmail.com>
2026-07-06 19:59:45 +0000

    Add test for accessing float properties

--- test/slint/component_instance_test.rb
+++ test/slint/component_instance_test.rb
@@ -16,16 +16,19 @@ module Slint
       assert_equal(@component_definition.name, @component_instance.definition.name)
     end
 
-    def test_property_accessor
+    def test_property_accessor # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
       assert_equal(42, @component_instance.get_property("int_property"))
+      assert_in_delta(110.0, @component_instance.get_property("float_prop"))
       assert_equal("test-string-value", @component_instance.get_property("text_prop"))
       assert(@component_instance.get_property("bool_prop"))
 
       @component_instance.set_property("int_property", 10)
+      @component_instance.set_property("float_prop", 10.5)
       @component_instance.set_property("text_prop", "new-string")
       @component_instance.set_property("bool_prop", false)
 
       assert_equal(10, @component_instance.get_property("int_property"))
+      assert_in_delta(10.5, @component_instance.get_property("float_prop"))
       assert_equal("new-string", @component_instance.get_property("text_prop"))
       refute(@component_instance.get_property("bool_prop"))
 
@@ -36,14 +39,17 @@ module Slint
 
     def test_global_property_accessor # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
       assert_equal(100, @component_instance.get_global_property("Glob", "my_global_property"))
+      assert_in_delta(30.3, @component_instance.get_global_property("Glob", "global_float_property"))
       assert_equal("global-string-value", @component_instance.get_global_property("Glob", "global_text_prop"))
       refute(@component_instance.get_global_property("Glob", "global_bool_prop"))
 
       @component_instance.set_global_property("Glob", "my_global_property", 200)
+      @component_instance.set_global_property("Glob", "global_float_property", 20.3)
       @component_instance.set_global_property("Glob", "global_text_prop", "new-global-string")
       @component_instance.set_global_property("Glob", "global_bool_prop", true)
 
       assert_equal(200, @component_instance.get_global_property("Glob", "my_global_property"))
+      assert_in_delta(20.3, @component_instance.get_global_property("Glob", "global_float_property"))
       assert_equal("new-global-string", @component_instance.get_global_property("Glob", "global_text_prop"))
       assert(@component_instance.get_global_property("Glob", "global_bool_prop"))
 
@@ -57,16 +63,17 @@ module Slint
 
     def source
       <<~SLINT
-        global Glob {
+        export global Glob {
           in-out property <int> my_global_property: 100;
+          in-out property <float> global_float_property: 30.3;
           in-out property <string> global_text_prop: "global-string-value";
           in-out property <bool> global_bool_prop: false;
           in-out property <color> global_col_prop: #ffaaff;
         }
 
-        export { Glob }
         export component App inherits Window {
           in-out property <int> int_property: 42;
+          in-out property <float> float_prop: 110;
           in-out property <string> text_prop: "test-string-value";
           in-out property <bool> bool_prop: true;
           in-out property <color> col_prop: #ffaaff;
