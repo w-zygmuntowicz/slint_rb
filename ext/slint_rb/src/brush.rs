7507d8e76c3fe65e564ce7913e2078ff2e2fd9de
Wojciech Zygmuntowicz <wzygmuntowicz.zygmuntowicz@gmail.com>
2026-07-12 16:11:18 +0200

    Brush type implementation (#1)
    * Update formatting of Rakefile
    
    * Rename transforming helper functions
    
    * Add test for accessing float properties
    
    * Return brush type when getting color type property
    
    * Implement is_transparent for color property
    
    * Implement default constructor and getters for color
    
    * Implement initializer from rgb
    
    * Implement initializer from argb
    
    * Implement parsing colors from hex
    
    * Implement color to string
    
    * Implement transparentize for Color
    
    * Refactor initializer
    
    * Implement mix for Color
    
    * Implement brighter for Color
    
    * Implement darker for Color
    
    * Implement with_alpha for Color
    
    * Extract errors module
    
    * Extract Color to brush module
    
    * Extract brush init function
    
    * Extract init to compiler module
    
    * Move Brush from compiler to brush mod
    
    * Implement brush from color constructor and color getter
    
    * Test brush transparent predicate
    
    * Implement opaque predicate for brush
    
    * Implement brighter for brush
    
    * Implement darker for brush
    
    * Implement transparentize for brush
    
    * Implement with_alpha for brush
    
    * Set sane defaults when defining a color with missing values

---
+++ ext/slint_rb/src/brush.rs
@@ -0,0 +1,184 @@
+use std::fmt;
+use magnus::prelude::*;
+use magnus::{function, method, typed_data, scan_args};
+
+use crate::errors::{RbResult, SlintError};
+
+#[magnus::wrap(class = "Slint::Color")]
+#[derive(Default, Debug, PartialEq, PartialOrd)]
+pub struct Color {
+    color: slint_interpreter::Color
+}
+
+impl From<slint_interpreter::Color> for Color {
+    fn from(color: slint_interpreter::Color) -> Self {
+        Self {
+            color: color
+        }
+    }
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.color.to_string())
+    }
+}
+
+impl Color {
+    pub fn new(maybe_input: &[magnus::Value]) -> RbResult<Self> {
+        let args = scan_args::scan_args::<(), (Option<String>,), (), (), magnus::RHash, ()>(maybe_input)?;
+        let (color_string,) = args.optional;
+        let kwargs = scan_args::get_kwargs::<_, (), (Option<u8>, Option<u8>, Option<u8>, Option<u8>), ()>(args.keywords, &[], &["red", "green", "blue", "alpha"])?;
+
+        let (red, green, blue, alpha) = kwargs.optional;
+
+        match (color_string, red, green, blue, alpha) {
+            (None, None, None, None, None) => Ok(Self::default()),
+            (None, maybe_red, maybe_green, maybe_blue, None) => {
+                Ok(Self::from_rgb_u8(
+                    maybe_red.unwrap_or(0),
+                    maybe_green.unwrap_or(0),
+                    maybe_blue.unwrap_or(0)
+                ))
+            },
+            (None, maybe_red, maybe_green, maybe_blue, Some(alpha)) => {
+                Ok(Self::from_argb_u8(
+                    alpha,
+                    maybe_red.unwrap_or(0),
+                    maybe_green.unwrap_or(0),
+                    maybe_blue.unwrap_or(0)
+                ))
+            },
+            (Some(hex), None, None, None, None) => {
+                hex
+                    .parse::<css_color_parser2::Color>()
+                    .map(|c| Self::from_argb_u8((c.a * 255.) as u8, c.r, c.g, c.b) )
+                    .map_err(|err| SlintError::new_err(err.to_string()))
+            },
+            _ => Err(SlintError::new_err("Provide either a hex string or RGB(A) keywords, not both.".to_string()))
+        }
+    }
+
+    fn from_rgb_u8(red: u8, green: u8, blue: u8) -> Self {
+        slint_interpreter::Color::from_rgb_u8(red, green, blue).into()
+    }
+
+    fn from_argb_u8(alpha: u8, red: u8, green: u8, blue: u8) -> Self {
+        slint_interpreter::Color::from_argb_u8(alpha, red, green, blue).into()
+    }
+
+    pub fn red(&self) -> u8 {
+        self.color.red()
+    }
+
+    pub fn green(&self) -> u8 {
+        self.color.green()
+    }
+
+    pub fn blue(&self) -> u8 {
+        self.color.blue()
+    }
+
+    pub fn alpha(&self) -> u8 {
+        self.color.alpha()
+    }
+
+    pub fn transparentize(&self, factor: f32) -> Color {
+        self.color.transparentize(factor).into()
+    }
+
+    pub fn brighter(&self, factor: f32) -> Color {
+        self.color.brighter(factor).into()
+    }
+
+    pub fn darker(&self, factor: f32) -> Color {
+        self.color.darker(factor).into()
+    }
+
+    pub fn mix(&self, other: &Color, factor: f32) -> Color {
+        self.color.mix(&other.color, factor).into()
+    }
+
+    pub fn with_alpha(&self, alpha: f32) -> Color {
+        self.color.with_alpha(alpha).into()
+    }
+}
+
+#[magnus::wrap(class = "Slint::Brush")]
+pub struct Brush {
+    brush: slint_interpreter::Brush
+}
+
+impl From<slint_interpreter::Brush> for Brush {
+    fn from(brush: slint_interpreter::Brush) -> Self {
+        Self {
+            brush: brush
+        }
+    }
+}
+
+impl Brush {
+    pub fn solid(color: &Color) -> Self {
+        slint_interpreter::Brush::SolidColor(color.color).into()
+    }
+
+    pub fn color(&self) -> Color {
+        self.brush.color().into()
+    }
+
+    pub fn is_transparent(&self) -> bool {
+        self.brush.is_transparent()
+    }
+
+    pub fn is_opaque(&self) -> bool {
+        self.brush.is_opaque()
+    }
+
+    pub fn brighter(&self, factor: f32) -> Self {
+        self.brush.brighter(factor).into()
+    }
+
+    pub fn darker(&self, factor: f32) -> Self {
+        self.brush.darker(factor).into()
+    }
+
+    pub fn transparentize(&self, amount: f32) -> Self {
+        self.brush.transparentize(amount).into()
+    }
+
+    pub fn with_alpha(&self, alpha: f32) -> Self {
+        self.brush.with_alpha(alpha).into()
+    }
+}
+
+pub fn init(ruby: &magnus::Ruby, slint_module: &magnus::RModule) -> RbResult<()> {
+    let color_class = slint_module.define_class("Color", ruby.class_object())?;
+    color_class.define_singleton_method("new", function!(Color::new, -1))?;
+    color_class.define_method("red", method!(Color::red, 0))?;
+    color_class.define_method("green", method!(Color::green, 0))?;
+    color_class.define_method("blue", method!(Color::blue, 0))?;
+    color_class.define_method("alpha", method!(Color::alpha, 0))?;
+    color_class.define_method("transparentize", method!(Color::transparentize, 1))?;
+    color_class.define_method("brighter", method!(Color::brighter, 1))?;
+    color_class.define_method("darker", method!(Color::darker, 1))?;
+    color_class.define_method("mix", method!(Color::mix, 2))?;
+    color_class.define_method("with_alpha", method!(Color::with_alpha, 1))?;
+    color_class.define_method("to_s", method!(Color::to_string, 0))?;
+    color_class.define_method("inspect", method!(<Color as typed_data::Inspect>::inspect, 0),)?;
+    // <=> sort operator based on Rust PartialOrd impl
+    color_class.define_method("<=>", method!(<Color as typed_data::Cmp>::cmp, 1))?;
+    // defines <, <=, >, >=, and == based on <=>
+    color_class.include_module(ruby.module_comparable())?;
+
+    let brush_class = slint_module.define_class("Brush", ruby.class_object())?;
+    brush_class.define_singleton_method("solid", function!(Brush::solid, 1))?;
+    brush_class.define_method("color", method!(Brush::color, 0))?;
+    brush_class.define_method("transparent?", method!(Brush::is_transparent, 0))?;
+    brush_class.define_method("opaque?", method!(Brush::is_opaque, 0))?;
+    brush_class.define_method("brighter", method!(Brush::brighter, 1))?;
+    brush_class.define_method("darker", method!(Brush::darker, 1))?;
+    brush_class.define_method("transparentize", method!(Brush::transparentize, 1))?;
+    brush_class.define_method("with_alpha", method!(Brush::with_alpha, 1))?;
+
+    Ok(())
+}
--- test/slint/component_instance_test.rb
+++ test/slint/component_instance_test.rb
@@ -22,6 +22,10 @@ module Slint
       assert_equal("test-string-value", @component_instance.get_property("text_prop"))
       assert(@component_instance.get_property("bool_prop"))
 
+      col_prop = @component_instance.get_property("col_prop")
+
+      assert_instance_of(Brush, col_prop)
+
       @component_instance.set_property("int_property", 10)
       @component_instance.set_property("float_prop", 10.5)
       @component_instance.set_property("text_prop", "new-string")
